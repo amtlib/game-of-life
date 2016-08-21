0a886cf879752926089f9aa23fa82200120eaf0e
Grzegorz Pach <maszeczuszets@protonmail.com>
2016-08-21 17:26:34 +0200

    made check function - applies 'game of life' rules, changed access to single cell.

--- cell.js
+++ cell.js
@@ -1,15 +1,15 @@
-function Cell(x, y, size){
+function Cell(x, y, size) {
     this.x = x;
     this.y = y;
     this.size = size;
     this.alive = false;
-
-    this.draw_me = function(){
+    this.alive_neighbors = 0;
+    this.draw_me = function () {
         noFill();
-        if(this.alive){
+        if (this.alive) {
             fill('#fff');
         }
         //noStroke();
-        rect(this.x*cell_size, this.y*cell_size, this.size, this.size);
+        rect(this.x * cell_size, this.y * cell_size, this.size, this.size);
     };
 }
--- script.js
+++ script.js
@@ -1,48 +1,89 @@
 var columns, rows;
 var board;
-var next_board;
-
+var next;
 var cell_size = 10;
 
-function setup(){
-    createCanvas(600,400);
-    rows = floor(height/cell_size);
-    columns = floor(width/cell_size);
-
+function setup() {
+    createCanvas(800, 800);
+    rows = floor(height / cell_size);
+    columns = floor(width / cell_size);
     //main board
     board = new Array(columns);
-
-    for(var i = 0; i < columns;i++){
+    for (var i = 0; i < columns; i++) {
         board[i] = new Array(rows);
     }
-    for(var i = 0;i<columns;i++){
-        for(var j = 0;j<rows;j++){
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
             board[i][j] = new Cell(i, j, cell_size);
         }
     }
     //next board
-    next = board.slice();
-init();
+    next = new Array(columns);
+    for (var i = 0; i < columns; i++) {
+        next[i] = new Array(rows);
+    }
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
+            next[i][j] = new Cell(i, j, cell_size);
+        }
+    }
+    init();
 }
-function init(){
-    //fills grid random values (alive or death)
 
-    for(var i = 0;i<columns;i++){
-        for(var j = 0;j<rows;j++){
-            //border's cells are always death
-            if(i==0 || j==0 || i == columns - 1 || j == rows - 1){
-                board[i][j].alive = false;
-            }else{
-                board[i][j].alive = random() > 0.5 ? true : false;
-            }
+function init() {
+    //fills grid with random values (alive or death)
+    //border's cells are always death
+    for (var i = 1; i < columns - 1; i++) {
+        for (var j = 1; j < rows - 1; j++) {
+            board[i][j].alive = random() > 0.5;
         }
     }
 }
-function draw(){
+
+function draw() {
     background('#212121');
-    for(var i = 0;i<columns;i++){
-        for(var j = 0;j<rows;j++){
+    check();
+    for (var i = 0; i < columns; i++) {
+        for (var j = 0; j < rows; j++) {
             board[i][j].draw_me();
         }
     }
+}
+
+function check() {
+    for (var i = 1; i < columns - 1; i++) {
+        for (var j = 1; j < rows - 1; j++) {
+            board[i][j].alive_neighbors = 0;
+            for (var x = -1; x < 2; x++) {
+                for (var y = -1; y < 2; y++) {
+                    if (board[i + x][j + y].alive) {
+                        board[i][j].alive_neighbors++;
+                    }
+                }
+            }
+            if (board[i][j].alive) {
+                board[i][j].alive_neighbors--;
+            }
+            //game of life rules
+            if (board[i][j].alive) {
+                if (board[i][j].alive_neighbors < 2 || board[i][j].alive_neighbors > 3) {
+                    next[i][j].alive = false;
+                }
+                else {
+                    next[i][j].alive = true;
+                }
+            }
+            else {
+                if (board[i][j].alive_neighbors == 3) {
+                    next[i][j].alive = true;
+                }
+                else {
+                    next[i][j].alive = false;
+                }
+            }
+        }
+    }
+    var temp = board;
+    board = next;
+    next = temp;
 }
