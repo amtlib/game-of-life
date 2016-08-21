36767f0d1e98591cbd05822bba3a09079a0b1bd3
Grzegorz Pach <maszeczuszets@protonmail.com>
2016-08-21 15:08:20 +0200

    cells are drawn on canvas

--- cell.js
+++ cell.js
@@ -2,4 +2,14 @@ function Cell(x, y, size){
     this.x = x;
     this.y = y;
     this.size = size;
+    this.alive = false;
+
+    this.draw_me = function(){
+        noFill();
+        if(this.alive){
+            fill('#fff');
+        }
+        //noStroke();
+        rect(this.x*cell_size, this.y*cell_size, this.size, this.size);
+    };
 }
--- script.js
+++ script.js
@@ -5,10 +5,11 @@ var next_board;
 var cell_size = 10;
 
 function setup(){
-    createCanvas(100,80);
-    rows = height/cell_size;
-    columns = width/cell_size;
+    createCanvas(600,400);
+    rows = floor(height/cell_size);
+    columns = floor(width/cell_size);
 
+    //main board
     board = new Array(columns);
 
     for(var i = 0; i < columns;i++){
@@ -16,10 +17,18 @@ function setup(){
     }
     for(var i = 0;i<columns;i++){
         for(var j = 0;j<rows;j++){
-            board[rows][columns] = new Cell(i, j, cell_size);
+            board[i][j] = new Cell(i, j, cell_size);
         }
     }
+    //next board
+    next = board.slice();
+
 }
 function draw(){
-
+    background('#212121');
+    for(var i = 0;i<columns;i++){
+        for(var j = 0;j<rows;j++){
+            board[i][j].draw_me();
+        }
+    }
 }
